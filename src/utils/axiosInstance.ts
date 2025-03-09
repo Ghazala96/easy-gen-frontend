5f6da67210bbf0caacf15cf5ce3b3cfbc61bd0f9
Marwan Ghazala <mghazala@soar.inc>
2025-03-10 01:22:09 +0200

    chore: Added env variables

--- src/utils/axiosInstance.ts
+++ src/utils/axiosInstance.ts
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const API_BASE_URL = 'http://localhost:3000'; //TODO: ENV variable
+const API_BASE_URL = import.meta.env.VITE_API_BASE_URL || 'http://localhost:3000';
 
 export const axiosInstance = axios.create({
   baseURL: API_BASE_URL,
